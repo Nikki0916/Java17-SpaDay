c2a53494dd94b7f29d403748a4c2735ccd5d5779
Nicole Wade <nwade0916@gmail.com.com>
2024-05-13 21:25:51 -0500

    completed studio

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,34 +1,37 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.UserData;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/add")
+    @GetMapping("")
     public String displayAddUserForm() {
-        return "user/add";
+        return "/user/add";
     }
 
-    @PostMapping
+    @PostMapping("")
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (!user.getPassword().equals(verify)) {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("error", "Passwords do not match.");
+            return "/user/add";
+        }
+        UserData.add(user);
         model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
-        }
-
+        model.addAttribute("users", UserData.getAll());
+        return "/user/index";
     }
 
-
+    @GetMapping("/details/{id}")
+    public String displayUserDetails(@PathVariable int id, Model model) {
+        model.addAttribute("user", UserData.getById(id));
+        return "/user/details";
+    }
 }
